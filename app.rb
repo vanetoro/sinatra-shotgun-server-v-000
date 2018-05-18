e9dc96cfa755356d39a2555254215c793e0a598d
Vanessa Toro <vanetoro84@gmail.com>
2018-05-18 22:16:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Started my server using Shotgun!"
+    "Started my server using Shotgun not Rackup!"
   end
 
 end
